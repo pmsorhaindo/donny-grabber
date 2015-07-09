5b3f40865ee51f2a911150884ce8602ed253c99d
Michael Sorhaindo <pmsorhaindo@gmail.com>
2015-07-09 10:03:16 +0100

    wip

--- donnyGrabber.js
+++ donnyGrabber.js
@@ -1,13 +1,13 @@
 var fetch = require('fetch-ponyfill')();
 
 function status(response) {
-	if (response.status === 200){
+	if (response.status === 200) {
 		return response;
 	}
 	throw new Error(response.statusText);
 }
 
-var loadComponent = function(resourceName, require, callback, config){
+var loadComponent = function(resourceName, require, callback, config) {
 
 	console.log('fetching donny component: ' + resourceName);
 
@@ -16,14 +16,29 @@ var loadComponent = function(resourceName, require, callback, config){
 		.then(function(response) {
 			return response.text();
 		})
-		.then(function(componentObj){
+		.then(function(componentObj) {
 			componentObj = JSON.parse(componentObj);
+			addCss(componentObj.css)
 			callback(componentObj.html);
 		})
-		.catch(function(err){
+		.catch(function(err) {
 			console.error(err + 'fetch for donny failed!');
 			callback(err.text());
 		});
 };
 
-module.exports = {load: loadComponent};
+function addCss(css) {
+	var head = document.getElementsByTagName('head')[0];
+	var s = document.createElement('style');
+	s.setAttribute('type', 'text/css');
+	if (s.styleSheet) { // IE
+		s.styleSheet.cssText = css;
+	} else { // the world
+		s.appendChild(document.createTextNode(css));
+	}
+	head.appendChild(s);
+}
+
+module.exports = {
+	load: loadComponent
+};
